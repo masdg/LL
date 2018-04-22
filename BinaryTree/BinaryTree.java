13432d06e0d7c96230140ccc980d00f3e615a3d1
Andrew <andrew@Andrews-MacBook-Air-2.local>
2018-04-21 23:40:09 -0700

    ui and add function

--- BinaryTree/BinaryTree.java
+++ BinaryTree/BinaryTree.java
@@ -11,7 +11,7 @@ public class BinaryTree {
     public static int lineNumber=0;
     private static final String FILE = "./classlist.txt";
     public static int pos = 0;
-
+    public static boolean exit = false;
 
 
     public static void main(String args[]){
@@ -114,9 +114,6 @@ public class BinaryTree {
         } // - for (int i = 0; i < binList.length; i++) {}
 
         Collections.shuffle(Arrays.asList(binList));
-        System.out.println(binList[0]);
-        System.out.println(binList[1]);
-
 
         Root r;
         r = new Root();
@@ -125,14 +122,85 @@ public class BinaryTree {
 
         while(ind < binList.length) {
 
-            System.out.println(ind);
             Node tmpR = new Node(binList[ind],binList[ind],binList[ind],ind);
             AddNode(ind, tmpR, r.root);
             ind++;
 
         }
 
-        StoreList(r.root);
+        while(!exit) { // runs the code until the user decides to stop the program
+
+            String func = "";
+            String str = "";
+            Node tmp = null;
+            String a1 = "";
+            String a2 = "";
+            String a3 = "";
+            String a4 = "";
+            String cumulative = "";
+            
+            System.out.println("\nTraversal (Inorder (in), Preorder (pre), Postorder (post), Breadthfirst (breadth), Add (a), Exit program (exit)):");
+
+            while(!func.toLowerCase().equals("in")&&!func.toLowerCase().equals("inorder")&&!func.toLowerCase().equals("post")&&!func.toLowerCase().equals("postorder")&&!func.toLowerCase().equals("pre")&&!func.toLowerCase().equals("preorder")&&!func.toLowerCase().equals("breadth")&&!func.toLowerCase().equals("breadthfirst")&&!func.toLowerCase().equals("a")&&!func.toLowerCase().equals("add")){
+            // get valid user input
+
+                func = getInput("Function");
+                if(exit) func = "in";
+
+            } // - while(!func.toLowerCase().equals("f")&&!func.toLowerCase().equals("find")&&!func.toLowerCase().equals("d")&&!func.toLowerCase().equals("delete")&&!func.toLowerCase().equals("a")&&!func.toLowerCase().equals("add")){}
+
+            if(exit) continue;
+
+            switch(func.toLowerCase()){ //executes code depending on the user input
+
+                case "in":    InTree(r.root);
+                break; // find case 1
+
+                case "inorder":   InTree(r.root);
+                break; // find case 2
+
+                case "breadth":  BreadthTree(r.root);
+                break; // delete case 1
+
+                case "breadthfirst":   BreadthTree(r.root);
+                break; // delete case 2
+
+                case "pre": PreTree(r.root);
+                break; // add case 1
+
+                case "preorder":   PreTree(r.root);
+                break; // add case 2
+
+                case "post":   PostTree(r.root);
+                break; // print case 1
+
+                case "postorder":   PostTree(r.root);
+                break; // print case 2
+
+                case "a":   a1 = getInput("Last Name");
+                            a2 = getInput("First Name");
+                            a3 = getInput("ID");
+                            a4 = getInput("Grade");
+                            cumulative = a3 + " " + a4 + " " + a1 + " " + a2;
+                            tmp = new Node(cumulative,cumulative,cumulative,ind);
+                            AddNode(ind, tmp, r.root);
+                            ind++;
+                            if(exit) func = "a";
+                            break;
+                case "add": a1 = getInput("Last Name");
+                            a2 = getInput("First Name");
+                            a3 = getInput("ID");
+                            a4 = getInput("Grade");
+                            cumulative = a3 + " " + a4 + " " + a1 + " " + a2;
+                            tmp = new Node(cumulative,cumulative,cumulative,ind);
+                            AddNode(ind, tmp, r.root);
+                            ind++;
+                            if(exit) func = "a";
+                            break;
+
+            } // - switch(func.toLowerCase()){}
+
+        } // - while(!exit) {
 
     } // - public static void main(String args[]){}
 
@@ -147,67 +215,29 @@ public class BinaryTree {
 
             if(String.valueOf(bin.charAt(p)).equals("0")) {
 
- //               System.out.print(curr.lastName.trim() + " ");
-
-                if(curr.left==null){curr.left = addedNode; System.out.print("yes ");}
+                if(curr.left==null)curr.left = addedNode;
                 else {
 
                     curr = curr.left;
-                    System.out.print(bin.charAt(p) + "left ");
 
-                }
+                } // - else {}
 
-            }
+            } // - if(String.valueOf(bin.charAt(p)).equals("0")) {}
+
             else if(String.valueOf(bin.charAt(p)).equals("1")) {
 
-//                System.out.print(curr.lastName + " ");
-
-                if(curr.right==null){curr.right = addedNode; System.out.print("yes ");}
+                if(curr.right==null)curr.right = addedNode;
                 else {
 
                     curr = curr.right;
-                    System.out.print(bin.charAt(p) + "right ");
 
-                }
+                } // - else {}
 
-            };
+            }; // - else if(String.valueOf(bin.charAt(p)).equals("1")) {}
 
-        }
+        } // - for(int p = 1; p < stringLength; p++){}
 
-        System.out.println(curr.lastName);
-
-    }
-
-    // public static void PrintTree(Node root, int maxSize){
-
-    //     for(int tracker = 2; tracker <= maxSize; tracker++) {
-
-    //         int currentIndex = 2;
-    //         Node curr = root;
-
-    //         while(currentIndex <= tracker){
-
-    //             if(toBinary(currentIndex).equals(0)) {
-
-    //                 curr = curr.left;
-
-    //             }
-
-    //             else{
-
-    //                 curr = curr.right;
-
-    //             }
-
-    //             currentIndex++;
-
-    //         }
-
-    //         System.out.println(curr.lastName);
-
-    //     }
-
-    // }
+    } // - public static void AddNode(int index, Node addedNode, Node root){}
 
     public static boolean BreadthFirst(Node tmp, ListElement tmpL, int orDepth, int depthCompare){
         
@@ -264,31 +294,6 @@ public class BinaryTree {
 
     } // - public static void Inorder(Node tmp, ListElement tmpL){}
 
-    public static void StoreList(Node tmp) {
-
-        Head h = new Head();
-        h.head = new ListElement("","","");
-        int d = 1;
-        boolean isFalse = false;
-
-        while(!isFalse) {
-            
-            if(!BreadthFirst(tmp, h.head, d, 1))isFalse = true;
-            d++;
-            System.out.println();
-            System.out.println();
-
-        }
-
-        System.out.println();
-        Inorder(tmp, h.head);
-        System.out.println();
-        Preorder(tmp, h.head);
-        System.out.println();
-        Postorder(tmp,h.head);
-
-    }
-
     public static void Preorder(Node tmp, ListElement tmpL){
         
         tmpL.lastName = tmp.lastName;
@@ -335,6 +340,51 @@ public class BinaryTree {
 
     } // - public static void Postorder(Node tmp, ListElement tmpL){}
 
+    public static void BreadthTree(Node tmp) {
+
+        Head h = new Head();
+        h.head = new ListElement("","","");
+        int d = 1;
+        boolean isFalse = false;
+
+        while(!isFalse) {
+            
+            if(!BreadthFirst(tmp, h.head, d, 1))isFalse = true;
+            d++;
+            System.out.println();
+            System.out.println();
+
+        }
+
+    }
+
+    public static void PostTree(Node tmp) {
+
+        Head h = new Head();
+        h.head = new ListElement("","","");
+        Postorder(tmp, h.head);
+        System.out.println();
+
+    }
+
+    public static void InTree(Node tmp) {
+
+        Head h = new Head();
+        h.head = new ListElement("","","");
+        Inorder(tmp, h.head);
+        System.out.println();
+
+    }
+
+    public static void PreTree(Node tmp) {
+
+        Head h = new Head();
+        h.head = new ListElement("","","");
+        Preorder(tmp, h.head);
+        System.out.println();
+
+    }
+
     public static String toBinary(int n) {
 
        if (n == 0) {
@@ -354,6 +404,28 @@ public class BinaryTree {
 
        return binary;
 
-   }
+    }
+
+    public static String getInput(String msg){ // get an input with a prompt at the start
+
+        String q = "";
+
+        try{
+
+            System.out.print(msg + " > ");
+            Scanner input = new Scanner(System.in);
+            q = input.nextLine();
+            if(q.toLowerCase().equals("exit")) exit = true; // exits when the user inputs "exit"
+
+        } // - try {}
+        catch(Exception e){
+
+            System.out.println("Invalid input");
+
+        } // - catch(Exception e){}
+
+        return q;
+
+    } // - public String getInput(){}
 
 } // - public class BinaryTree {}
